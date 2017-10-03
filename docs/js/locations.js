b114995c99bc0b7a072b9ff720ded371965541dc
Dmitriy Dzyuba <development2@performancefoundry.com>
2017-10-03 21:17:22 +0300

    Separate JSON data into a file

---
+++ docs/js/locations.js
@@ -0,0 +1,34 @@
+var locations = [
+    {
+        title: 'Aristocrate',
+        location: { lat: 47.8444084, lng: 35.1277231 }
+    },
+    {
+        title: 'Mushroom Lounge Bar',
+        location: { lat: 47.8390437, lng: 35.1219503 }
+    },
+    {
+        title: 'Smoky | ZP',
+        location: { lat: 47.8460164, lng: 35.1125332 }
+    },
+    {
+        title: 'AMBAR',
+        location: { lat: 47.8229409, lng: 35.1682036 }
+    },
+    {
+        title: 'Villa Oliva',
+        location: { lat: 47.846656, lng: 35.108290 }
+    },
+    {
+        title: 'Giusti Espresso Bar',
+        location: { lat: 47.8619893, lng: 35.0280993 }
+    },
+    {
+        title: 'Bella Italia',
+        location: { lat: 47.8246499, lng: 35.1633466 }
+    },
+    {
+        title: 'Fiesta',
+        location: { lat: 47.8510769, lng: 35.1102636 }
+    }
+];
